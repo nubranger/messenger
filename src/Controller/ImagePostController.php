14240ba5f964535dcb53a8f66f2236bae744e816
nubranger <mtviraga@gmail.com>
2021-07-06 01:00:39 +0300

    Envelopes & Stamps

--- src/Controller/ImagePostController.php
+++ src/Controller/ImagePostController.php
@@ -14,8 +14,10 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\MessageBus;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\DelayStamp;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -61,7 +63,10 @@ class ImagePostController extends AbstractController
         $entityManager->flush();
 
         $message = new AddPonkaToImage($imagePost->getId());
-        $messageBus->dispatch($message);
+        $envelope = new Envelope($message, [
+            new DelayStamp(500)
+        ]);
+        $messageBus->dispatch($envelope);
 
         return $this->toJson($imagePost, 201);
     }
