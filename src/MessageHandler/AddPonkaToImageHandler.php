29563b058a537477d9d8d4a01529d2d1ded28e3d
nubranger <mtviraga@gmail.com>
2021-07-06 01:42:10 +0300

    Investigating & Retrying Failed Messages ( php bin/console messenger )

--- src/MessageHandler/AddPonkaToImageHandler.php
+++ src/MessageHandler/AddPonkaToImageHandler.php
@@ -44,7 +44,7 @@ class AddPonkaToImageHandler implements MessageHandlerInterface, LoggerAwareInte
             return;
         }
 
-        if (rand(0, 10) < 7 || true) {
+        if (rand(0, 10) < 7) {
             throw new \Exception("I failed randomly!!!!");
         }
 
