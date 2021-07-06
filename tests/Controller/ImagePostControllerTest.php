8271c2fa0012eedbda1a8ef9593f995ced1a630a
nubranger <56818312+nubranger@users.noreply.github.com>
2021-07-07 00:13:14 +0300

    Functional Test for the Upload Endpoint ( composer require phpunit --dev, php bin/phpunit )

---
+++ tests/Controller/ImagePostControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Messenger\Transport\InMemoryTransport;
+
+class ImagePostControllerTest extends WebTestCase
+{
+    public function testCreate(): void
+    {
+        $client = static::createClient();
+
+        $uploadedFile = new UploadedFile(
+            __DIR__.'/../fixtures/ryan-fabien.jpg',
+            'ryan-fabien.jpg'
+        );
+        $client->request('POST', '/api/images', [], [
+            'file' => $uploadedFile
+        ]);
+
+        self::assertResponseIsSuccessful();
+    }
+}
