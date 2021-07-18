d360c2e194aad9c233a82bd253988c127d36f855
nubranger <mtviraga@gmail.com>
2021-07-18 22:12:57 +0300

    AMQP with RabbitMQ

--- src/MessageHandler/Command/AddPonkaToImageHandler.php
+++ src/MessageHandler/Command/AddPonkaToImageHandler.php
@@ -44,9 +44,9 @@ class AddPonkaToImageHandler implements MessageHandlerInterface, LoggerAwareInte
             return;
         }
 
-        if (rand(0, 10) < 7) {
-            throw new \Exception("I failed randomly!!!!");
-        }
+//        if (rand(0, 10) < 7) {
+//            throw new \Exception("I failed randomly!!!!");
+//        }
 
         $updatedContents = $this->ponkaficator->ponkafy(
             $this->photoManager->read($imagePost->getFilename())
