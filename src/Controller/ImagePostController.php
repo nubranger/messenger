85349d4c9e85acf4751a8feabad698c21e615fcb
nubranger <mtviraga@gmail.com>
2021-07-19 00:12:37 +0300

    Transport for Consuming External Messages

--- src/Controller/ImagePostController.php
+++ src/Controller/ImagePostController.php
@@ -73,7 +73,7 @@ class ImagePostController extends AbstractController
         ]);
         $messageBus->dispatch($envelope);
 
-        $messageBus->dispatch(new LogEmoji(2));
+//        $messageBus->dispatch(new LogEmoji(2));
 
         return $this->toJson($imagePost, 201);
     }
